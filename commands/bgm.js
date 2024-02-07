36e476e8ccda859237df1f05638bf8ab1f4bd2bb
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-02-07 20:44:19 +0500

    🍁┃ᴜsᴍᴀɴ-ᴍᴅ ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ┃ 🍁

--- commands/bgm.js
+++ commands/bgm.js
@@ -1,3 +1,23 @@
+/**
+========================================================
+  _   _ ____  __  __    _    _   _       __  __ ____  
+ | | | / ___||  \/  |  / \  | \ | |     |  \/  |  _ \ 
+ | | | \___ \| |\/| | / _ \ |  \| |_____| |\/| | | | |
+ | |_| |___) | |  | |/ ___ \| |\  |_____| |  | | |_| |
+  \___/|____/|_|  |_/_/   \_\_| \_|     |_|  |_|____/ 
+                                                                                                          
+========================================================
+ Copyright (C) 2022.                                                                                        
+ Licensed under the  GPL-3.0 License;                                                      
+ You may not use this file except in compliance with the License.    
+ It is supplied in the hope that it may be useful                                     
+ * @project_name : USMAN-MD                                                                    
+ * @author : USMAN-SER <https://github.com/USMAN-SER>   
+ * @description : USMAN-MD ,A Multi-functional whatsapp bot.       
+ * @version 2.0.1                                                                                             
+ ========================================================
+ **/
+
 const {TelegraPh , bgms  } = require('../lib/')
 
 
