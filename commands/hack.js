c5762e791978e47b2434e7c79159d334947b301f
USMAN-SER <141281506+USMAN-SER@users.noreply.github.com>
2024-02-07 20:07:22 +0500

    🍁┃ᴜsᴍᴀɴ-ᴍᴅ ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ┃ 🍁

--- commands/hack.js
+++ commands/hack.js
@@ -90,7 +90,7 @@ citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
 
 await sleep(1000)
 
-            return citel.reply('*ALL FILES TRANSFERRED*');
+            return citel.reply('*ALL FILES TRANSFERRED BY USMAN-MD*');
 
         }
 
